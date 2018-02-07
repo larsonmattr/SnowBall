2196296aca42a21e7e7191a94324e6fa392d7388
Matt Larson <larsonmattr@gmail.com>
2018-02-06 21:56:17 -0600

    Added UtahTerrain/Scene for longer testing. Updated the SkiController physics to rely more on forces from poles, forces from kinetic -> potential energy.  No longer using a constant force in a direction.  Improved falling in air, and working on sideways forces for skids.

--- Assets/Snowball/Scripts/SkiController.cs
+++ Assets/Snowball/Scripts/SkiController.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class SkiController : MonoBehaviour {
 
-	float rate = 0.0f;
-	float maxRate = 30f;
-	float convertFactor = 1f;
-	float lastY = 0f;
+	float convertFactor = 1000f;
+	float pushFactor = 10000f;
+	float frictionFactor = 100f;
 
 	// The HMD angles are used for controlling the turns.
 	public GameObject hmd;
@@ -15,6 +14,8 @@ public class SkiController : MonoBehaviour {
 	public GameObject rightSki;
 
 	public Rigidbody body;
+
+	private float lastY = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -33,7 +34,30 @@ public class SkiController : MonoBehaviour {
 	 *  Ski pole collisions will cause forward movements.
 	 */
 	public void HandleSkiPoleCollision() {
-		rate += 5f;
+		//rate += 5f;
+		Debug.Log("Push!");
+
+		// Movement should go to ski player's forward vector in World Space.
+		Vector3 forward = gameObject.transform.forward;
+		Vector3 force = forward * pushFactor;
+		body.AddForce (force);
+	}
+
+	/**
+	 * Calculate the height above the terrain
+	 */
+	private float getHeightOverTerrain() {
+		RaycastHit hit;
+		float heightAboveGround = 0;
+
+		Transform t = gameObject.transform;
+
+		Vector3 p = (leftSki.transform.position + rightSki.transform.position) / 2f;
+
+		if (Physics.Raycast (p, t.TransformDirection (Vector3.down), out hit)) {
+			heightAboveGround = hit.distance;
+		}
+		return heightAboveGround;
 	}
 
 	// Update is called once per frame
@@ -41,9 +65,19 @@ public class SkiController : MonoBehaviour {
 	}                                 
 
 	void FixedUpdate () {
+
+		// TODO: replace the height check to see if on the ground, with 
+		// collision detection checking against the hit with "Terrain"
+
+		// Ski moves should only rotate if we are on the ground.
+		float height = getHeightOverTerrain ();
+		// Debug.Log ("height is " + height);
 	
 		// If we are moving, allow the ski(s) and player to rotate based on HMD roll.
-		if (rate > 4f) {
+		// This may need to be changed to Lerp to the correct orientation, not snap.
+		float speed = body.velocity.magnitude;
+		//Debug.Log ("speed is " + speed);
+		if (speed > 4f) {
 			// Get the roll from euler angles.
 			float z = hmd.transform.localRotation.eulerAngles.z;
 
@@ -63,29 +97,60 @@ public class SkiController : MonoBehaviour {
 			leftSki.transform.Rotate(new Vector3(0,rotationFactor, 0 ));
 			rightSki.transform.Rotate(new Vector3(0,rotationFactor, 0));
 
-			float rot = rotationFactor * Time.deltaTime;
-			gameObject.transform.Rotate (new Vector3 (0, rot, 0));
+			// If close to ground, allow to turn the player's direction.
+			if (height < 0.5f) {
+				float rot = rotationFactor * Time.deltaTime;
+				gameObject.transform.Rotate (new Vector3 (0, rot, 0));
+			}
 		}
 
 		// Movement should go to ski player's forward vector in World Space.
 		Vector3 forward = gameObject.transform.forward;
 
 		// Only move if above speed.
-		if (rate > 4f) {
+		if (height < 0.5f) {
+
+			// Descending the hill, convert potential energy into forward movements.
+			float potentialToKinetic = 9.8f * (lastY - gameObject.transform.position.y) * convertFactor;
 
 			// A force that will apply in a direction
-			Vector3 force = forward * rate * 40f;
+			Vector3 force = forward * potentialToKinetic;
 			body.AddForce (force);
+
+			// TODO: sideways movements giving friction forces...
+			/*
+			// Apply a sideways force opposite vector when moving in direction at tangent to skis.
+			// This should also kick up snow as an effect.
+			Vector3 sidewaysFriction = getSlideFriction(forward, body.velocity);
+			body.AddRelativeForce (sidewaysFriction * frictionFactor);
+
+			Debug.Log ("Friction force is " + sidewaysFriction.magnitude);
+			*/
 		}
 
-		// Descending the hill, convert potential energy into forward movements.
-		rate += 9.8f * (lastY - gameObject.transform.position.y) * convertFactor;
 		lastY = gameObject.transform.position.y;
-
-		// Keep the rate from becoming too high.
-		if (rate > maxRate)
-			rate = maxRate;
-
 	}
 		
+	// Find the magnitude of friction from sliding sideways.
+	Vector3 getSlideFriction(Vector3 Forward, Vector3 Velocity) {
+
+		Vector3 ForwardUnit = Forward;
+		ForwardUnit.Normalize ();
+
+		Vector3 VelocityUnit = Velocity;
+		VelocityUnit.Normalize ();
+
+		// 0 = perpendicular, 1 = parallel, -1 opposite
+		float dotProduct = Vector3.Dot(ForwardUnit, VelocityUnit);
+		
+		if (dotProduct >= 0 && dotProduct < 1) {
+			// Calculate sideways vector magnitude.
+			float speed = Forward.magnitude * (1 - dotProduct);
+			Vector3 difference = ForwardUnit - VelocityUnit;
+			difference *= speed;
+			return difference;
+		}
+
+		return new Vector3(0,0,0);
+	}
 }
